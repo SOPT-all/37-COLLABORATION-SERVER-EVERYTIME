bb76f6a791680df6d35c0a2621d064df0973dd1a
Chae-Yu <chaeyu2004@naver.com>
2025-11-17 16:27:41 +0900

    refactor: 사용하지 않은 에러 코드 삭제

--- src/main/java/com/everytime/global/exception/constant/GlobalErrorCode.java
+++ src/main/java/com/everytime/global/exception/constant/GlobalErrorCode.java
@@ -5,8 +5,6 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum GlobalErrorCode implements ErrorCode {
-    FILE_INIT_FAILED(HttpStatus.INTERNAL_SERVER_ERROR.value(), "FILE_INIT_FAILED", "파일 초기화를 실패했습니다."),
-    FILE_UPDATE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR.value(), "FILE_UPDATE_FAILED", "데이터 파일 저장에 실패하였습니다."),
     INVALID_JSON(HttpStatus.BAD_REQUEST.value(), "INVALID_JSON", "잘못된 JSON 형식입니다."),
     TYPE_MISMATCH(HttpStatus.BAD_REQUEST.value(), "TYPE_MISMATCH", "요청 타입이 올바르지 않습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "INTERNAL_SERVER_ERROR", "서버 내부 오류가 발생했습니다.");
@@ -15,11 +13,9 @@ public enum GlobalErrorCode implements ErrorCode {
     private final String code;
     private final String msg;
 
-
     GlobalErrorCode(int httpStatus, String code, String msg) {
         this.httpStatus = httpStatus;
         this.code = code;
         this.msg = msg;
     }
-
 }
