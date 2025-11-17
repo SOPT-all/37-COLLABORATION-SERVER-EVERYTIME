3b890936e3a983e2ad0a5f8830342c5bd3b981aa
Chae-Yu <chaeyu2004@naver.com>
2025-11-17 17:39:18 +0900

    feat: 수정 시간 추가

--- src/main/java/com/everytime/global/entity/BaseTimeEntity.java
+++ src/main/java/com/everytime/global/entity/BaseTimeEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -16,4 +17,7 @@ public class BaseTimeEntity {
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    protected LocalDateTime updatedAt;
 }
