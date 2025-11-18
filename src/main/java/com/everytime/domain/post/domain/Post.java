fc768e9c9db6b1590d076d295e77500c703fcf70
Chae-Yu <chaeyu2004@naver.com>
2025-11-18 15:57:19 +0900

    refactor: Getter 어노테이션 추가

--- src/main/java/com/everytime/domain/post/domain/Post.java
+++ src/main/java/com/everytime/domain/post/domain/Post.java
@@ -4,9 +4,11 @@ import com.everytime.domain.post.domain.enums.Category;
 import com.everytime.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post extends BaseTimeEntity {
 
