324010e2c293a93799c34415271ec5838a81de95
earl9rey <earl9rey@naver.com>
2025-11-19 00:33:07 +0900

    refactor: 강의평 조회 성공 코드 변경

--- src/main/java/com/everytime/global/exception/constant/ReviewSuccessCode.java
+++ src/main/java/com/everytime/global/exception/constant/ReviewSuccessCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ReviewSuccessCode implements SuccessCode {
 
-    GET_ALL_REVIEWS(HttpStatus.OK.value(), "강의평 조회 성공");
+    GET_RECENT_REVIEWS(HttpStatus.OK.value(), "최근 강의평 조회 성공");
 
     private final int status;
     private final String msg;
