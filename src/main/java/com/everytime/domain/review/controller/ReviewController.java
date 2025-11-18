5c9e51f808f441564258622404f54cf2c85f929c
earl9rey <earl9rey@naver.com>
2025-11-18 22:56:40 +0900

    refactor: ReviewController에 ReviewSuccessCode 적용

--- src/main/java/com/everytime/domain/review/controller/ReviewController.java
+++ src/main/java/com/everytime/domain/review/controller/ReviewController.java
@@ -2,6 +2,7 @@ package com.everytime.domain.review.controller;
 
 import com.everytime.domain.review.dto.response.ReviewResponse;
 import com.everytime.domain.review.service.ReviewService;
+import com.everytime.global.exception.constant.ReviewSuccessCode;
 import com.everytime.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,16 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
 @RequiredArgsConstructor
+@RestController
 @RequestMapping("/review")
 public class ReviewController {
 
     private final ReviewService reviewService;
 
     @GetMapping
-    public BaseResponse<List<ReviewResponse>> getReviews() {
-        List<ReviewResponse> reviews = reviewService.getReviews();
-        return BaseResponse.ok("강의평 조회 성공", reviews);
+    public BaseResponse<List<ReviewResponse>> getAllReviews() {
+        List<ReviewResponse> response = reviewService.getReviews();
+        return BaseResponse.ok(ReviewSuccessCode.GET_ALL_REVIEWS.getMsg(), response);
     }
 }
