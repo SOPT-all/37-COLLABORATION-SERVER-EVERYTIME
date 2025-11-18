ab891c94408ce7b60bb3ee04670a89586b188115
Chae-Yu <chaeyu2004@naver.com>
2025-11-18 15:28:23 +0900

    feat: Post 도메인 작성

---
+++ src/main/java/com/everytime/domain/post/domain/Post.java
@@ -0,0 +1,30 @@
+package com.everytime.domain.post.domain;
+
+import com.everytime.domain.post.domain.enums.Category;
+import com.everytime.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+    private int likeCount;
+    private int commentCount;
+    @Column(nullable = false)
+    private Boolean isAnonymous;
+    @Column(nullable = false, length = 50)
+    private String nickname;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private Category category;
+}
