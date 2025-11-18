e79d85175a04c2cac3bd6f5064914e9a7ef8aa1c
Chae-Yu <chaeyu2004@naver.com>
2025-11-18 16:00:57 +0900

    feat: 게시물 관련 성공 코드 추가

---
+++ src/main/java/com/everytime/global/exception/constant/PostSuccessCode.java
@@ -0,0 +1,17 @@
+package com.everytime.global.exception.constant;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum PostSuccessCode implements SuccessCode {
+    GET_ALL_POSTS_BY_CATEGORY(HttpStatus.OK.value(), "카테고리 별 상위 4개 게시글 조회 완료");
+
+    private final int status;
+    private final String msg;
+
+    PostSuccessCode(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+}
