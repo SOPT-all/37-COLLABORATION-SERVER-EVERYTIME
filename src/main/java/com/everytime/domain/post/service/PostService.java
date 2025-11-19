77d7056b6e7e30d5d1201f7e5e98cf8027a26aef
Chae-Yu <chaeyu2004@naver.com>
2025-11-19 10:24:50 +0900

    refactor: 트랙잭션 어노테이션 추가

--- src/main/java/com/everytime/domain/post/service/PostService.java
+++ src/main/java/com/everytime/domain/post/service/PostService.java
@@ -7,12 +7,14 @@ import com.everytime.domain.post.dto.response.PostSummaryResponse;
 import com.everytime.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PostService {
     private final PostRepository postRepository;
 
