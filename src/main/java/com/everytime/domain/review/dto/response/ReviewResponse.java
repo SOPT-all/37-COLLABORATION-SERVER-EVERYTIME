be2cbc361edf5220e546ebbbd404e4a103fd686e
earl9rey <earl9rey@naver.com>
2025-11-19 12:11:01 +0900

    refactor: ReviewResponse에 id 필드 추가

--- src/main/java/com/everytime/domain/review/dto/response/ReviewResponse.java
+++ src/main/java/com/everytime/domain/review/dto/response/ReviewResponse.java
@@ -7,13 +7,14 @@ import lombok.Getter;
 @Getter
 @Builder
 public class ReviewResponse {
-
+    private Long id;
     private int rate;
     private String lecture;
     private String content;
 
     public static ReviewResponse from(Review review) {
         return ReviewResponse.builder()
+                .id(review.getId())
                 .rate(review.getRate())
                 .lecture(review.getLecture())
                 .content(review.getContent())
