c3dad985ed29fbf932b6643f21ef37b1143802af
earl9rey <earl9rey@naver.com>
2025-11-19 12:08:06 +0900

    refactor: ReviewService 조회 메서드에 @Transactional(readOnly = true) 추가

--- src/main/java/com/everytime/domain/review/service/ReviewService.java
+++ src/main/java/com/everytime/domain/review/service/ReviewService.java
@@ -4,10 +4,12 @@ import com.everytime.domain.review.dto.response.ReviewResponse;
 import com.everytime.domain.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ReviewService {
 
