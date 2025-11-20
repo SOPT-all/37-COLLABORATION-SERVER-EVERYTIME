af003279a488cba88bf3d56eee4d22b52a10d0c5
Chae-Yu <chaeyu2004@naver.com>
2025-11-20 21:34:42 +0900

    feat: 실시간 게시물 비어있는 경우 응답 enum 추가

--- src/main/java/com/everytime/global/exception/constant/PostSuccessCode.java
+++ src/main/java/com/everytime/global/exception/constant/PostSuccessCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum PostSuccessCode implements SuccessCode {
     GET_ALL_POSTS_BY_CATEGORY(HttpStatus.OK.value(), "카테고리 별 상위 4개 게시글 조회 완료"),
     GET_REALTIME_POST(HttpStatus.OK.value(), "실시간 인기 게시글 조회 완료"),
+    REALTIME_POST_EMPTY(HttpStatus.OK.value(), "실시간 인기 게시글이 없습니다."),
     GET_HOT_POSTS(HttpStatus.OK.value(), "핫 게시물 조회 완료");
 
     private final int status;
