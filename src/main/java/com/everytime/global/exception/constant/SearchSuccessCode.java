425d7e9fcdb6383d0a735be7fa17200cc853932f
earl9rey <earl9rey@naver.com>
2025-11-20 20:21:34 +0900

    feat: 검색 결과가 없을 시 성공 코드 추가

--- src/main/java/com/everytime/global/exception/constant/SearchSuccessCode.java
+++ src/main/java/com/everytime/global/exception/constant/SearchSuccessCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum SearchSuccessCode implements SuccessCode {
 
-    GET_POST_SEARCH_RESULT(HttpStatus.OK.value(), "게시글 검색 결과 조회 완료");
+    GET_POST_SEARCH_RESULT(HttpStatus.OK.value(), "게시글 검색 결과 조회 완료"),
+    GET_POST_SEARCH_EMPTY(HttpStatus.OK.value(), "검색 결과가 없습니다.");
 
     private final int status;
     private final String msg;
