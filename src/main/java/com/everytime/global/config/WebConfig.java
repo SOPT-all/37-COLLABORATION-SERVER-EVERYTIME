97a6368cf6ad7b102890ba69dff30180fea2e57c
Chae-Yu <chaeyu2004@naver.com>
2025-11-24 21:20:28 +0900

    chore: 프론트 CORS 설정

--- src/main/java/com/everytime/global/config/WebConfig.java
+++ src/main/java/com/everytime/global/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
