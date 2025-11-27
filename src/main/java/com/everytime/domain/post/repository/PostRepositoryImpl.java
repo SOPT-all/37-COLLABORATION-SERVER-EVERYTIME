41cab07bafb78b1cbc6913e2fd0bb6742e0c1ece
Chae-Yu <chaeyu2004@naver.com>
2025-11-27 18:04:30 +0900

    feat: QueryDSL 구현

---
+++ src/main/java/com/everytime/domain/post/repository/PostRepositoryImpl.java
@@ -0,0 +1,49 @@
+package com.everytime.domain.post.repository;
+
+import com.everytime.domain.post.domain.Post;
+import com.everytime.domain.post.domain.QPost;
+import com.everytime.domain.post.domain.enums.Category;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class PostRepositoryImpl implements PostRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+    private static final QPost post = QPost.post;
+
+    @Override
+    public List<Post> findTop4ByCategoryOrderByCreatedAtDesc(Category category) {
+        return jpaQueryFactory
+                .selectFrom(post)
+                .where(post.category.eq(category))
+                .orderBy(post.createdAt.desc())
+                .limit(4)
+                .fetch();
+    }
+
+    @Override
+    public Optional<Post> findTopByOrderByLikeCountDescCreatedAtDesc() {
+        Post result = jpaQueryFactory
+                .selectFrom(post)
+                .orderBy(post.likeCount.desc(), post.createdAt.desc())
+                .limit(1)
+                .fetchOne();
+
+        return Optional.ofNullable(result);
+    }
+
+    @Override
+    public List<Post> findTop4ByOrderByLikeCountDescCreatedAtDesc() {
+        return jpaQueryFactory
+                .selectFrom(post)
+                .orderBy(
+                        post.likeCount.desc(), post.createdAt.desc()
+                )
+                .limit(4)
+                .fetch();
+    }
+}
